c319991a7e0917e85ad1925a1665cf13095f3017
USERNAME <USERNAME@example.com>
2022-06-04 14:23:40 +0300

    part 3 - blog details and add to wishlist button

--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -1,18 +1,37 @@
-import { Col } from "reactstrap";
+import { Col, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 function Post({ post }) {
+	const [showButton, setShowButton] = useState(false);
 	return (
-		<Col xs='12' sd='6'>
-			<img
-				src={`https://picsum.photos/seed/${post.id}/600`}
-				style={{
-					height: "300px",
-					objectFit: "cover",
-					width: "100%",
-					marginTop: "50px",
-				}}
-			/>
-			<h2>{post.title}</h2>
-			<p>{post.body}</p>
+		<Col
+			xs='12'
+			sd='6'
+			onMouseEnter={() => {
+				setShowButton(true);
+			}}
+			onMouseLeave={() => {
+				setShowButton(false);
+			}}>
+			<Link
+				to={"/blog/" + post.id}
+				style={{ textDecoration: "none", color: "black" }}>
+				<img
+					src={`https://picsum.photos/seed/${post.id}/600`}
+					style={{
+						height: "300px",
+						objectFit: "cover",
+						width: "100%",
+						marginTop: "50px",
+					}}
+				/>
+				<h2>{post.title}</h2>
+				<p>{post.body}</p>
+			</Link>
+			<div style={{ minHeight: "50px" }}>
+				{showButton && <Button outline>Add to wishlist</Button>}
+				<Button outline>Ad to wishlist</Button>
+			</div>
 		</Col>
 	);
 }
--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,7 +1,77 @@
 import { useParams } from "react-router-dom";
+import { Container, Row, Col, Button } from "reactstrap";
+import { useState, useEffect } from "react";
+import Footer from "../common/Footer";
+import "./Blog.css";
 function Blog() {
 	const params = useParams();
-	return <h1>Pagina Blog- {params.blogId}</h1>;
+	const [post, setPost] = useState(null);
+	const [user, setUser] = useState(null);
+
+	const getBlog = async (blogId) => {
+		const responseData = await fetch(
+			"https://jsonplaceholder.typicode.com/posts/" + blogId
+		);
+		const apiPost = await responseData.json();
+		setPost(apiPost);
+	};
+
+	const getUser = async (userId) => {
+		const responseData = await fetch(
+			"https://jsonplaceholder.typicode.com/users/" + userId
+		);
+		const apiUser = await responseData.json();
+		setUser(apiUser);
+	};
+
+	useEffect(() => {
+		if (params && params.blogId) {
+			getBlog(params.blogId);
+		}
+	}, [params]);
+
+	useEffect(() => {
+		if (post && post.userId) {
+			getUser(post.userId);
+		}
+	}, [post]);
+
+	return (
+		<>
+			<Container className='mb-4'>
+				{post && user ? (
+					<>
+						<Row>
+							<img
+								className='post_cover'
+								src={`https://picsum.photos/seed/${post.id}/1000`}
+							/>
+						</Row>
+
+						<Row>
+							<h1>{post.title}</h1>
+							<h3 style={{ minHeight: "500px" }}>{post.body}</h3>
+							<Button className='mt-4 mb-4'>Ad to wishlist</Button>
+						</Row>
+						<Row>
+							<div className='author_container mt-4'>
+								<img src={`https://robohash.org/&{user.id}.png?set=set4`} />
+								<div className='mt-4'>
+									<h3>{user.name}</h3>
+									<h4>{user.email}</h4>
+									<h4>{user.website}</h4>
+									<h4>{user.phone}</h4>
+								</div>
+							</div>
+						</Row>
+					</>
+				) : (
+					<div>Loading...</div>
+				)}
+			</Container>
+			<Footer />
+		</>
+	);
 }
 
 export default Blog;
